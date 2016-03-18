2189fc6741b07de26d403f4e1f523f8128dcb3ad
Sagar.Gandhi <sagar.gandhi@feisystems.com>
2016-03-18 14:08:59 -0400

    Create mhcOauthLogin directive and oauth login template
    #8406 - As a staff admin, when I login to the MHC, the security protocols automatically authorize access to MHC apps

---
+++ client/app/security/directives/adminOauthLogin.directive.js
@@ -0,0 +1,58 @@
+/**
+ * Created by sagar.gandhi on 3/18/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.security')
+        .directive('mhcOauthLogin', mhcOauthLogin);
+
+    function mhcOauthLogin() {
+
+        var directive = {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'app/security/directives/oauthLogin.html',
+            scope: {},
+            bindToController: {},
+            controller: OauthLoginController,
+            controllerAs: 'oauthLoginVm'
+        };
+
+        return directive;
+
+        /* @ngInject */
+        function OauthLoginController(utilityService, authenticationService, oauthConfig, profileService, tokenService) {
+            var vm = this;
+            vm.login = login;
+            vm.canSubmit = canSubmit;
+
+            function login() {
+                authenticationService.login(vm.user.email, vm.user.password)
+                    .then(
+                        function (response) {
+                            tokenService.setToken(response);
+                            profileService.loadProfile()
+                                .then(
+                                    function (data) {
+                                        profileService.setProfile(data);
+                                        utilityService.redirectTo(oauthConfig.loginSuccessPath);
+                                    },
+                                    function (error) {
+                                        vm.profileError = true;
+                                    }
+                                );
+                        }, function (error) {
+                            tokenService.removeToken();
+                            vm.loginError = true;
+                        }
+                    );
+            }
+
+            function canSubmit(loginForm) {
+                return loginForm.$dirty && loginForm.$valid;
+            }
+        }
+    }
+})();
