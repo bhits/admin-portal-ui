ba293f1b0f57c243a8431feff939b8c103942094
tomson.ngassa@feisystems.com <tomson.ngassa@feisystems.com>
2016-03-31 23:16:39 -0400

    Fix the issue in the date not being pass when you update
    #8405 - As a staff admin, I can select a patient and initiate the user account creation [UI]

--- client/app/core/directives/mhcFormatDate.directive.js
+++ client/app/core/directives/mhcFormatDate.directive.js
@@ -14,7 +14,11 @@
                     link: function(scope, element, attrs, ngModelController) {
                         ngModelController.$parsers.push(function(data) {
                             //convert data from view format to model format
-                            return data;
+                            if(angular.isDefined(data)){
+                                return $filter('date')(data, 'MM/dd/yyyy');
+                            }else {
+                                return "";
+                            }
                         });
 
                         ngModelController.$formatters.push(function(data) {
