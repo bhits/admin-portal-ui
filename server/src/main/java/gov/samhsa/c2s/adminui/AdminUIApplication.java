c39325e93d97c5d90a85db07caa02a207037a460
sheng.li <sheng.li@feisystems.com>
2016-08-17 15:50:04 -0400

    change brand from mhc to c2s
    #10467 - As a developer, I will complete rebranding changes in code

--- server/src/main/java/gov/samhsa/c2s/adminui/AdminUIApplication.java
+++ server/src/main/java/gov/samhsa/c2s/adminui/AdminUIApplication.java
@@ -1,4 +1,4 @@
-package gov.samhsa.bhits.adminui;
+package gov.samhsa.c2s.adminui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
