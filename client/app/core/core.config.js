fb050f7462ce2c5740967c30e6f8b13a1a23ed76
JiahaoLi <Jiahao.Li@feisystems.com>
2016-09-15 16:47:49 -0400

    Change the restriction to the First and Last name for patient
    Bug#10887 - Admin Portal :: Intermitantly getting 500 error when trying to create a new patient in QA.

--- client/app/core/core.config.js
+++ client/app/core/core.config.js
@@ -12,6 +12,6 @@
                 ZIPCODE_REGEXP: /(^\d{5}$)|(^\d{5}-\d{4}$)/,
                 EMAIL_REGEXP: /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/,
                 PHONE_NUMBER_REGEXP: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-                FIRSTNAMELASTNAME_REGEXP: /^[a-zA-ZÀ-ÿ]+[-]?[a-zA-ZÀ-ÿ]*[a-zA-ZÀ-ÿ]$/
+                FIRSTNAMELASTNAME_REGEXP: /^[a-zA-ZÀ-ÿ]+[-]?[a-zA-ZÀ-ÿ']*[a-zA-ZÀ-ÿ]$/
             });
 })();
