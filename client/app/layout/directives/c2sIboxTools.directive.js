b95f1de8627d3ea98203dc183cef136e3ee3ab2d
JiahaoLi <Jiahao.Li@feisystems.com>
2016-09-22 17:20:44 -0400

    Format and clean up layout module
    #10926 - As a developer, I will have prepared admin-portal-ui,  patient-portal-ui code to make it more deployable by community member (HIE)

--- client/app/layout/directives/c2sIboxTools.directive.js
+++ client/app/layout/directives/c2sIboxTools.directive.js
@@ -3,71 +3,67 @@
 
     angular
         .module('app.layout')
-            .directive('c2sIboxTools', c2sIboxTools);
+        .directive('c2sIboxTools', c2sIboxTools);
 
-            /* @ngInject */
-            function c2sIboxTools() {
-                var directive = {
-                    restrict: 'A',
-                    scope: {},
-                    bindToController: {
-                        total: '@',
-                        c2sCollapsed: '=?',
-                        c2sChevronPreventDefault: '=?'
-                    },
-                    templateUrl: 'app/layout/directives/iBoxTools.html',
-                    controller: IboxToolController,
-                    controllerAs: 'iboxToolVm'
+    /* @ngInject */
+    function c2sIboxTools() {
+        var directive = {
+            restrict: 'A',
+            scope: {},
+            bindToController: {
+                total: '@',
+                c2sCollapsed: '=?',
+                c2sChevronPreventDefault: '=?'
+            },
+            templateUrl: 'app/layout/directives/iBoxTools.html',
+            controller: IboxToolController,
+            controllerAs: 'iboxToolVm'
+        };
+        return directive;
+    }
 
-                };
+    /* @ngInject */
+    function IboxToolController($scope, $element, $timeout) {
+        var iboxToolVm = this;
+        iboxToolVm.c2sChevronPreventDefault = !!iboxToolVm.c2sChevronPreventDefault;
+        var ibox = $element.closest('div.ibox');
+        var icon = $element.find('i:first');
+        var content = ibox.find('div.ibox-content');
+        iboxToolVm.toggleCollapsed = toggleCollapsed;
 
-                return directive;
+        $scope.$watch('iboxToolVm.c2sCollapsed', collapseWatchHandler);
+        activate();
+
+        function activate() {
+            if (iboxToolVm.c2sCollapsed) {
+                content.slideUp(0);
             }
+        }
 
-            /* @ngInject */
-            function IboxToolController($scope, $element, $timeout) {
-                var iboxToolVm = this;
-                iboxToolVm.c2sChevronPreventDefault = !!iboxToolVm.c2sChevronPreventDefault;
-                var ibox = $element.closest('div.ibox');
-                var icon = $element.find('i:first');
-                var content = ibox.find('div.ibox-content');
-                iboxToolVm.toggleCollapsed = toggleCollapsed;
-
-                $scope.$watch('iboxToolVm.c2sCollapsed',collapseWatchHandler);
-
-                activate();
-
-                function activate(){
-                    if (iboxToolVm.c2sCollapsed) {
-                        content.slideUp(0);
-                    }
-                }
-
-                function toggleCollapsed() {
-                    if (!iboxToolVm.c2sChevronPreventDefault) {
-                        iboxToolVm.c2sCollapsed = !iboxToolVm.c2sCollapsed;
-                    }
-                }
-
-
-                function collapseWatchHandler (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        showhide();
-                    }
-                }
-
-                function showhide() {
-                    content.slideToggle(200);
-                    // Toggle icon from up to down
-                    icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-                    resize();
-                }
-
-                function resize(){
-                    $timeout(function () {
-                        ibox.resize();
-                        ibox.find('[id^=map-]').resize();
-                    }, 50);
-                }
+        function toggleCollapsed() {
+            if (!iboxToolVm.c2sChevronPreventDefault) {
+                iboxToolVm.c2sCollapsed = !iboxToolVm.c2sCollapsed;
             }
+        }
+
+        function collapseWatchHandler(newValue, oldValue) {
+            if (newValue !== oldValue) {
+                showhide();
+            }
+        }
+
+        function showhide() {
+            content.slideToggle(200);
+            // Toggle icon from up to down
+            icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+            resize();
+        }
+
+        function resize() {
+            $timeout(function () {
+                ibox.resize();
+                ibox.find('[id^=map-]').resize();
+            }, 50);
+        }
+    }
 })();
