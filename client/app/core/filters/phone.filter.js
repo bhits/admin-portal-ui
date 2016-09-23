632be081a4bba7505641d7631b75b7745f4c53cb
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2016-09-23 11:46:24 -0400

    Format some modules .
    #10926 - As a developer, I will have prepared admin-portal-ui,  patient-portal-ui code to make it more deployable by community member (HIE)

--- client/app/core/filters/phone.filter.js
+++ client/app/core/filters/phone.filter.js
@@ -18,10 +18,10 @@
             if (angular.isDefined(numInt) && angular.isNumber(numInt) && numInt.toString().length >= 10) {
                 var numString = numInt.toString();
                 num = numString.slice(0, 3) + "-" +
-                      numString.slice(3, 6) + "-" +
-                      numString.slice(6, 10) +
-                      (numString.length > 10 ? '-' +
-                      numString.slice(10) : '');
+                    numString.slice(3, 6) + "-" +
+                    numString.slice(6, 10) +
+                    (numString.length > 10 ? '-' +
+                    numString.slice(10) : '');
             }
             return num;
         }
