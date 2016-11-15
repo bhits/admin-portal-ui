5e1acfe466f25cf96abd63f724e0e06f20e655ae
JiahaoLi <Jiahao.Li@feisystems.com>
2016-11-15 12:21:06 -0500

    Implement to get branding logo from server side
    #11661 - As a developer, I will have upgrade UI projects to get configuration at run time

--- client/app/account/controllers/login.controller.js
+++ client/app/account/controllers/login.controller.js
@@ -12,5 +12,10 @@
         vm.version = envService.version;
         vm.brandName = configService.getBrandName();
         vm.altLogoText = configService.getBrandInitials() + " Logo";
+        vm.brandLargeLogo = specifyImageType(configService.getBrandLargeLogo());
+
+        function specifyImageType(base64Image) {
+            return "data:image/png;base64," + base64Image;
+        }
     }
 })();
