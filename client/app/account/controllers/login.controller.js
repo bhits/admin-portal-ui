2eb014bb4f88eae40df901238aac657d8315a1b9
Shruti Rao <shruti.rao@feisystems.com>
2017-03-15 17:57:27 -0400

    Bug Fix:  Clicking on "Select Language" does not change to the selected language on the login page
    #12892:As a developer, I will have released internationalization (Spanish) C2S version 2.1.1

--- client/app/account/controllers/login.controller.js
+++ client/app/account/controllers/login.controller.js
@@ -7,7 +7,12 @@
         .controller('LoginController', LoginController);
 
     /* @ngInject */
-    function LoginController(envService, configService) {
+    function LoginController(envService,
+                             configService,
+                             $scope,
+                             $translate,
+                             tmhDynamicLocale,
+                             paginationConfig) {
         var vm = this;
         vm.version = envService.version;
         vm.brandName = configService.getBrandName();
@@ -16,6 +21,42 @@
 
         function specifyImageType(base64Image) {
             return "data:image/png;base64," + base64Image;
+        }
+
+        // Change language dynamically
+        var lang = window.localStorage.lang || 'en';
+
+        setDynamicLanguage(lang);
+        $scope.changeLanguage = function (language) {
+            $translate.use(language);
+            window.localStorage.lang = language;
+            setDynamicLanguage(language);
+            //window.location.reload();
+        };
+
+        function setDynamicLanguage (language) {
+            if (language === null || language.length < 2) {
+                tmhDynamicLocale.set("en");
+            } else {
+                tmhDynamicLocale.set (language.substring(0,2));
+                setTextofPagination (language.substring(0,2));
+            }
+        }
+
+        //translate the text of pagination of bootstrap
+        function setTextofPagination(language) {
+            if (language === "es"){
+                paginationConfig.firstText = "Inicio";
+                paginationConfig.previousText = "Anterior";
+                paginationConfig.lastText = "Fin";
+                paginationConfig.nextText = "Próximo";
+            } else if (language === "en"){
+                // note the different effects between REFRESH and choosing LANGUAGE
+                paginationConfig.firstText = "First";
+                paginationConfig.previousText = "Previous";
+                paginationConfig.lastText = "Last";
+                paginationConfig.nextText = "Next";
+            }
         }
     }
 })();
